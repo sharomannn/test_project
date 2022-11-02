4fc311b16059482f624ccaac7c0911254375965d
sharomannn <pvp380@gmail.com>
2022-11-02 18:13:41 +0500

    #1 Создал модели и настроил их в админке.

--- emergency_service/admin.py
+++ emergency_service/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from emergency_service import models
 
-# Register your models here.
+
+@admin.register(models.EmergencyService)
+class EmergencyService(admin.ModelAdmin):
+    list_display = ('name', 'code')
+
+
+@admin.register(models.Applicant)
+class Applicant(admin.ModelAdmin):
+    list_display = ('name', 'number')
+
+
+@admin.register(models.Appeal)
+class Appeal(admin.ModelAdmin):
+    list_display = ('date', 'number')
--- emergency_service/models.py
+++ emergency_service/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class EmergencyService(models.Model):
+    name = models.CharField("Название службы", max_length=100)
+    code = models.CharField("Код службы", max_length=100)
+    number = models.DecimalField("Номер телефона", max_digits=12, decimal_places=0)
+
+    class Meta:
+        verbose_name = 'Экстренная служба'
+        verbose_name_plural = 'Экстренные службы'
+        ordering = ['code']
+
+    def __str__(self):
+        return self.name
+
+
+class Applicant(models.Model):
+    name = models.CharField("ФИО заявителя", max_length=100)
+    date = models.DateField("Дата рождения")
+    health_status = models.TextField("Состояние здоровья")
+    number = models.CharField("Номер телефона", max_length=100)
+
+    class Meta:
+        verbose_name = 'Заявитель'
+        verbose_name_plural = 'Заявители'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Appeal(models.Model):
+    date = models.DateField("Дата обращения")
+    number = models.IntegerField("Номер обращения")
+    service = models.ManyToManyField(EmergencyService, related_name='services', verbose_name='Экстренная служба')
+    applicant = models.ForeignKey(Applicant, related_name='applicants', verbose_name='Заявитель', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Обращение'
+        verbose_name_plural = 'Обращения'
+        ordering = ['date', 'number']
+
