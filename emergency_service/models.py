ada964a18aaa89619b36801fe707d47f8da07856
sharomannn <pvp380@gmail.com>
2022-11-03 13:33:22 +0500

    #1 Добавил выбор поля для заявителя "Пол" с двумя значениями

--- emergency_service/models.py
+++ emergency_service/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 class EmergencyService(models.Model):
     name = models.CharField("Название службы", max_length=100)
     code = models.CharField("Код службы", max_length=100)
-    number = models.DecimalField("Номер телефона", max_digits=12, decimal_places=0)
+    number = models.DecimalField("Номер телефона", max_digits=12,
+                                 decimal_places=0)
 
     class Meta:
         verbose_name = 'Экстренная служба'
@@ -15,10 +17,18 @@ class EmergencyService(models.Model):
 
 
 class Applicant(models.Model):
+    MAN = 'М'
+    WOMAN = 'Ж'
+    GENDER_CHOICES = [
+        (MAN, 'Мужчина'),
+        (WOMAN, 'Женщина'), ]
     name = models.CharField("ФИО заявителя", max_length=100)
+    gender = models.CharField(max_length=2,
+                              choices=GENDER_CHOICES,
+                              default=WOMAN)
     date = models.DateField("Дата рождения")
-    health_status = models.TextField("Состояние здоровья")
-    number = models.CharField("Номер телефона", max_length=100)
+    health_status = models.TextField("Состояние здоровья", blank=True)
+    number = models.CharField("Номер телефона", max_length=100, blank=True)
 
     class Meta:
         verbose_name = 'Заявитель'
@@ -32,11 +42,13 @@ class Applicant(models.Model):
 class Appeal(models.Model):
     date = models.DateField("Дата обращения")
     number = models.IntegerField("Номер обращения")
-    service = models.ManyToManyField(EmergencyService, related_name='services', verbose_name='Экстренная служба')
-    applicant = models.ForeignKey(Applicant, related_name='applicants', verbose_name='Заявитель', on_delete=models.CASCADE)
+    service = models.ManyToManyField(EmergencyService, related_name='appeals',
+                                     verbose_name='Экстренная служба')
+    applicant = models.ForeignKey(Applicant, related_name='appeals',
+                                  verbose_name='Заявитель',
+                                  on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Обращение'
         verbose_name_plural = 'Обращения'
         ordering = ['date', 'number']
-
