1bc71385473d58647c66b828b9150d77040ac58d
sharomannn <pvp380@gmail.com>
2022-11-03 15:30:55 +0500

    #1 Добавил значение статус обращения, количество пострадавших, поле "Не звонить"

--- emergency_service/models.py
+++ emergency_service/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class EmergencyService(models.Model):
     name = models.CharField("Название службы", max_length=100)
     code = models.CharField("Код службы", max_length=100)
-    number = models.DecimalField("Номер телефона", max_digits=12,
-                                 decimal_places=0)
+    number = models.CharField("Номер телефона", max_length=100)
 
     class Meta:
         verbose_name = 'Экстренная служба'
@@ -23,8 +22,9 @@ class Applicant(models.Model):
     GENDER_CHOICES = [
         (MAN, 'Мужчина'),
         (WOMAN, 'Женщина'), ]
+    photo_applicant = models.ImageField(blank=True)
     name = models.CharField("ФИО заявителя", max_length=100)
-    gender = models.CharField(max_length=2,
+    gender = models.CharField("Пол", max_length=2,
                               choices=GENDER_CHOICES,
                               default=WOMAN)
     date = models.DateField("Дата рождения")
@@ -43,14 +43,23 @@ class Applicant(models.Model):
 
 
 class Appeal(models.Model):
-    date = models.DateField("Дата обращения")
-    number = models.UUIDField("Номер обращения", primary_key=True,
-                              default=uuid.uuid4, editable=True)
+    IN_WORK = 'WR'
+    FINNISHED = 'FN'
+    STATUS_CHOICES = [
+        (IN_WORK, 'В работе'),
+        (FINNISHED, 'Завершено'), ]
+    status_appeal = models.CharField("Статус обращения", max_length=2,
+                              choices=STATUS_CHOICES,
+                              default=IN_WORK)
+    date = models.DateField("Дата обращения", auto_now_add=True )
+    number = models.UUIDField('Номер обращения', default=uuid.uuid4, editable=True)
     service = models.ManyToManyField(EmergencyService, related_name='appeals',
                                      verbose_name='Экстренная служба')
     applicant = models.ForeignKey(Applicant, related_name='appeals',
                                   verbose_name='Заявитель',
                                   on_delete=models.CASCADE)
+    number_cases = models.IntegerField('Количество пострадавших', default=1)
+    not_call = models.BooleanField('Не звонить', default=False)
 
     class Meta:
         indexes = [
