b1da50732ac8e8d1aad7f6ae1f3661b67791a121
sharomannn <pvp380@gmail.com>
2022-11-07 12:12:56 +0500

    #2 Сделал поиск и сортировку для обращений и заявителей

--- emergency_service/admin.py
+++ emergency_service/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from emergency_service import models
 
 
-
 @admin.register(models.EmergencyService)
 class EmergencyService(admin.ModelAdmin):
     list_display = ('name', 'code')
@@ -10,10 +9,15 @@ class EmergencyService(admin.ModelAdmin):
 
 @admin.register(models.Applicant)
 class Applicant(admin.ModelAdmin):
-    list_display = ('name', 'number')
+    search_fields = ('surname', 'name', 'name_father', 'date')
+    list_display = ('surname', 'name', 'name_father', 'number', 'date')
 
 
 @admin.register(models.Appeal)
 class Appeal(admin.ModelAdmin):
+    ordering = ('-date',)
+    date_hierarchy = 'date'
+    list_display_links = ('date', 'number')
     list_filter = ('date', 'status_appeal')
-    list_display = ('date', 'number')
+    list_display = ('date', 'number', 'applicant')
+    filter_horizontal = ('service',)
